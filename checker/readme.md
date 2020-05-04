e517777aec6721cc807afedbdbace76539714c0f
DiriiMQ <minhquang116@gmail.com>
2020-05-04 10:53:37 +0700

    sync

---
+++ checker/readme.md
@@ -0,0 +1,5 @@
+Trong folder checker chi co:
+- Duy nhat 1 file cpp
+- N folder test cases
+- 5 file cpp
+Huong dan su dung: run file run.py, diem o file Score.log
--- cmd/cmd.py
+++ cmd/cmd.py
@@ -1,4 +1,6 @@
 import subprocess
+import os
 
+os.system('ls -l')
 list_files = subprocess.run(["ls", "-l"])
 print("The exit code was: %d" % list_files.returncode)
